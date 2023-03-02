1e4face31f4bd06217160aa67c39a6fbad890104
Rohit Kumar <86485476+Kumarohit45@users.noreply.github.com>
2023-03-02 14:32:45 +0530

    Update Improving-Accuracy-using-Convolutions.py

--- Course-1/Week-3/Improving-Accuracy-using-Convolutions.py
+++ Course-1/Week-3/Improving-Accuracy-using-Convolutions.py
@@ -1,1 +1,21 @@
+import tensorflow as tf
+fmnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
 
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.summary()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+print('\nMODEL TRAINING\n')
+model.fit(training_images, training_labels, epochs=20)
+print('\nMODEL EVALUATION\n')
+model.evaluate(test_images, test_labels)
